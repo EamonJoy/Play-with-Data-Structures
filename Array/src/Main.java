5850a7d738223df4e4342a4f2309737a87ba3d58
赵依民 <18510512011@163.com>
2018-12-13 14:19:19 +0800

    数组结束

--- Array/src/Main.java
+++ Array/src/Main.java
@@ -2,17 +2,40 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int[] scores = new int[]{56,12,54,100};
+//        int[] scores = new int[]{56,12,54,100};
+//
+//        for (int i = 0; i < scores.length; i++) {
+//            System.out.println(scores[i]);
+//        }
+//
+//        System.out.println();
+//
+//        scores[2] = 100;
+//        for (int score: scores) {
+//            System.out.println(score);
+//        }
 
-        for (int i = 0; i < scores.length; i++) {
-            System.out.println(scores[i]);
+
+        Array<Integer> array = new Array<>();
+        for (int i = 0; i < 10; i++) {
+            array.addLast(i);
         }
+        System.out.println(array);
 
-        System.out.println();
+        array.addLast(100);
+        array.addFirst(99);
+        System.out.println(array);
 
-        scores[2] = 100;
-        for (int score: scores) {
-            System.out.println(score);
-        }
+        array.removeFirst();
+        array.removeElement(100);
+        System.out.println(array);
+
+
+//        Array<Student> studentArray = new Array<>();
+//        studentArray.addLast(new Student("张三",70));
+//        studentArray.addLast(new Student("李四",80));
+//        studentArray.addLast(new Student("王五",90));
+//        System.out.println(studentArray);
+
     }
 }
