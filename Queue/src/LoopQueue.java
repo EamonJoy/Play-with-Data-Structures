e3a9aded1cad679d2819327cb229d8b5a1cfbadb
赵依民 <18510512011@163.com>
2018-12-18 17:33:31 +0800

    完善动态队列结构

--- Queue/src/LoopQueue.java
+++ Queue/src/LoopQueue.java
@@ -19,17 +19,36 @@ public class LoopQueue<E> implements Queue<E>  {
 
     @Override
     public void enqueue(E e) {
+        if ((tail + 1) % data.length == front) {
+            resize(getCapacity() * 2);
+        }
 
+        data[tail] = e;
+        tail  = (tail + 1) % data.length;
+        size ++;
     }
 
     @Override
     public E dequeue() {
-        return null;
+        if (isEmpty())
+            throw  new IllegalArgumentException("Cannot dequeue from an empty queue.");
+
+        E res = data[front];
+        data[front] = null;
+        front = (front + 1) % data.length;
+        size--;
+
+        if (size <= getCapacity() / 4 && getCapacity() / 2 != 0)
+            resize(getCapacity() / 2);
+
+        return res;
     }
 
     @Override
     public E getFront() {
-        return null;
+        if (isEmpty())
+            throw  new IllegalArgumentException("Queue is empty");
+        return data[front];
     }
 
     @Override
@@ -44,5 +63,29 @@ public class LoopQueue<E> implements Queue<E>  {
 
     public int getCapacity() {
         return data.length - 1;
+    }
+
+    private void resize(int newCapaticy) {
+        E[] newData = (E[])new Object[newCapaticy + 1];
+        for (int i = 0; i < size; i++) {
+            newData[i] = data[(i + front) % data.length];
+        }
+        data = newData;
+        front = 0;
+        tail = size;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder res = new StringBuilder();
+        res.append(String.format("Queue: Size = %d , capacity = %d\n",size,getCapacity()));
+        res.append("front [");
+        for (int i = front; i != tail; i = (i + 1) % data.length) {
+            res.append(data[i]);
+            if ((i+1) % data.length != tail)
+                res.append(",");
+        }
+        res.append("] tail");
+        return res.toString();
     }
 }
--- Queue/src/Main.java
+++ Queue/src/Main.java
@@ -1,16 +1,60 @@
+import java.util.Random;
+
 public class Main {
 
-    public static void main(String[] args) {
-        ArrayQueue<Integer> arrayQueue = new ArrayQueue<>();
+    private static double testQueue(Queue<Integer> q,int opCount) {
 
-        for (int i = 0; i < 10; i++) {
-            arrayQueue.enqueue(i);
-            System.out.println(arrayQueue);
+        long startTime = System.nanoTime();
 
-            if (i % 3 == 2) {
-                arrayQueue.dequeue();
-                System.out.println(arrayQueue);
-            }
+        Random random = new Random();
+        for (int i = 0; i < opCount; i++) {
+            q.enqueue(i);
         }
+        for (int i = 0; i < opCount; i++) {
+            q.dequeue();
+        }
+
+        long endTime = System.nanoTime();
+
+        return (endTime - startTime) / 1000000000.0;
+    }
+
+    public static void main(String[] args) {
+//        ArrayQueue<Integer> arrayQueue = new ArrayQueue<>();
+//
+//        for (int i = 0; i < 10; i++) {
+//            arrayQueue.enqueue(i);
+//            System.out.println(arrayQueue);
+//
+//            if (i % 3 == 2) {
+//                arrayQueue.dequeue();
+//                System.out.println(arrayQueue);
+//            }
+//        }
+
+//        LoopQueue<Integer> loopQueue = new LoopQueue<>();
+//
+//        for (int i = 0; i < 11; i++) {
+//            loopQueue.enqueue(i);
+//            System.out.println(loopQueue);
+//
+//            if (i % 3 > 0) {
+//                loopQueue.dequeue();
+//                System.out.println(loopQueue);
+//            }
+//        }
+
+
+        int opCount = 100000;
+
+        ArrayQueue<Integer> arrayQueue = new ArrayQueue<>();
+        double time1 = testQueue(arrayQueue,opCount);
+        System.out.println("ArrayQueue time：" + time1 + " s");
+
+
+        LoopQueue<Integer> loopQueue = new LoopQueue<>();
+        double time2 = testQueue(loopQueue,opCount);
+        System.out.println("LoopQueue time：" + time2 + " s");
+
     }
 }
